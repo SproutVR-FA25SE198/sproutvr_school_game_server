9a0a4d9e9af4ea7c19dda0f5678cd5a1d48cd377
anonyfriday <duykimvu75@gmail.com>
2025-10-12 13:45:16 +0700

    feat(logging): add logging pipeline #3

---
+++ SproutVRSchool.Application/Abstractions/Clock/IDateTimeProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SproutVRSchool.Application.Abstractions.Clock;
+
+public interface IDateTimeProvider
+{
+    DateTimeOffset UtcDateTimeNow { get; }
+    DateTimeOffset VietNamDateTimeNow { get; }
+}
---
+++ SproutVRSchool.Infrastructure/Clock/DateTimeProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SproutVRSchool.Application.Abstractions.Clock;
+
+namespace SproutVRSchool.Infrastructure.Clock;
+
+internal class DateTimeProvider : IDateTimeProvider
+{
+    // ============================
+    // === Zones
+    // ============================
+
+    private static readonly TimeZoneInfo VietNamTimeZone = TimeZoneInfo.FindSystemTimeZoneById("SE Asia Standard Time");
+
+    // ============================
+    // === Methods
+    // ============================
+
+    public DateTimeOffset UtcDateTimeNow => DateTimeOffset.UtcNow;
+
+    public DateTimeOffset VietNamDateTimeNow => TimeZoneInfo.ConvertTime(DateTimeOffset.UtcNow, VietNamTimeZone);
+}
