f13c326ef74687d0cec837696bfac6c326eae0ea
anonyfriday <duykimvu75@gmail.com>
2025-10-29 18:17:10 +0700

    feat(vrlessons): add validatior for design vr lesson #30

--- SproutVRSchool.Application/RequestHandlers/VRLessons/CreateVRLesson/CreateVRLessonTaskRequestDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SproutVRSchool.Application.RequestHandlers.VRLessons.CreateVRLesson;
-
-public record CreateVRLessonTaskRequestDto(
-    Guid TaskLocationId,
-    Guid MapObjectId,
-    Guid ActivityTypeId,
-    int TaskNumber,
-    string Description
-);
