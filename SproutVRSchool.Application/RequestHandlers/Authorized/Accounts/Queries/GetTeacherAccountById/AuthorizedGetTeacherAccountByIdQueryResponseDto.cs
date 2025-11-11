fc979d69cfa54af4155bbb18d121c3518c7f6242
anonyfriday <duykimvu75@gmail.com>
2025-11-11 18:09:08 +0700

    feat(lessons): add get account by id #7

---
+++ SproutVRSchool.Application/RequestHandlers/Authorized/Accounts/Queries/GetTeacherAccountById/AuthorizedGetTeacherAccountByIdQueryResponseDto.cs
@@ -0,0 +1,27 @@
+﻿using SproutVRSchool.Application.Commons.Responses;
+
+namespace SproutVRSchool.Application.RequestHandlers.Authorized.Accounts.Queries.GetTeacherAccountById;
+
+public sealed record AuthorizedGetTeacherAccountByIdQueryResponseDto(
+    Guid TeacherId,
+    string Email,
+    string FullName,
+    StatusDto Status,
+    IReadOnlyList<string> Roles,
+    DateOnly? DateOfBirth,
+    DateTimeOffset JoinedAtUtc,
+    IReadOnlyList<AuthorizedGetTeacherAccountByIdLessonResponseDto> Lessons,
+    IReadOnlyList<AuthorizedGetTeacherAccountByIdVRLearningSessionResponseDto> VRLearningSessions
+);
+
+public sealed record AuthorizedGetTeacherAccountByIdLessonResponseDto(
+    Guid LessonId,
+    string Name,
+    string Status
+);
+
+public sealed record AuthorizedGetTeacherAccountByIdVRLearningSessionResponseDto(
+    Guid SessionId,
+    string ClassName,
+    DateTimeOffset CreatedAtUtc
+);
